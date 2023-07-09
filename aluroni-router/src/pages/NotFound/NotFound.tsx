ac9d4b22c4f98c470bb674cddd7c9281ea4bdd71
Henrriky <tectutors.123@gmail.com>
2023-07-09 16:39:46 -0300

    Funcionality of route notfound

---
+++ aluroni-router/src/pages/NotFound/NotFound.tsx
@@ -0,0 +1,16 @@
+import styles from './NotFound.module.scss';
+import stylesTema from 'styles/Tema.module.scss';
+
+import { ReactComponent as NotFoundImage } from 'assets/not-found/not_found.svg';
+import classNames from 'classnames';
+
+export default function NotFound() {
+  return (
+    <div className={classNames({
+      [styles.container]: true,
+      [stylesTema.container]: true 
+    })}>
+      <NotFoundImage />
+    </div>
+  );
+}
