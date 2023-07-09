a02ad16fc68bee7cea290a2b4dc9517548b840ba
Henrriky <tectutors.123@gmail.com>
2023-07-09 16:10:45 -0300

    Inicio style

--- aluroni-router/src/pages/Inicio/index.tsx
+++ aluroni-router/src/pages/Inicio/index.tsx
@@ -1,6 +1,7 @@
 import cardapio from 'data/cardapio.json';
 import styles from './Inicio.module.scss';
 import stylesTema from 'styles/Tema.module.scss';
+import nossaCasa from 'assets/nossa_casa.png';
 
 export default function Inicio() {
   let pratosRecomendados = [...cardapio];
@@ -12,13 +13,20 @@ export default function Inicio() {
         {pratosRecomendados.map(item => (
           <div key={item.id} className={styles.recomendado}>
             <div className={styles.recomendado__imagem}>
-              <img src={item.photo} alt={item.title}/>
+              <img src={item.photo} alt={item.title} />
             </div>
             <button className={styles.recomendado__botao}>
               Ver mais
             </button>
           </div>
         ))}
+      </div>
+      <h3 className={stylesTema.titulo}> Nossa Casa </h3>
+      <div className={styles.nossaCasa}>
+        <img src={nossaCasa} alt="Casa do aluroni" />
+        <div className={styles.nossaCasa__endereco}>
+          Rua Vergueiro, 3185 <br /><br /> Vila Mariana - SP
+        </div>
       </div>
     </section>
   );
